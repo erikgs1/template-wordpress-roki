c7ce4cc571c96920f33d604ae40760bdf30d032e
erikgs1 <erik.contato15@gmail.com>
2020-07-14 01:55:38 -0300

    terminei a página Nossa historia

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,13 @@ console.log("Load script.js");
 
 // Instantiating the global app object
 var app = {};
+
+// jQuery('.main-menu .ul > li').mouseenter(function() {
+//     console.log('menu > li');
+
+//     var submenuHeight = jQuery(this).find('.sub-menu').height();
+
+//     jQuery(this).height(submenuHeight);
+
+//     console.log('submenuHeight ', submenuHeight);
+// })
